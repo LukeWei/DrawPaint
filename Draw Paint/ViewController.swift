c0954f9c22a95ddafd44947148a83423e527a59b
Luke Lin <z00728326@gmail.com>
2020-06-11 16:36:14 +0800

    Initial Commit

---
+++ Draw Paint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Draw Paint
+//
+//  Created by Luke Lin on 2020/6/11.
+//  Copyright © 2020 Luke Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
